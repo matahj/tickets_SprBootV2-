024614c6416be75e79754fe9b13cd79fce0120bf
JorgeMataH <matahj@gmail.com>
2023-01-29 18:04:41 -0600

    Manejo de errores

--- src/main/java/com/bedu/tickets_sprbootv2/controllers/UserController.java
+++ src/main/java/com/bedu/tickets_sprbootv2/controllers/UserController.java
@@ -3,8 +3,10 @@ package com.bedu.tickets_sprbootv2.controllers;
 import com.bedu.tickets_sprbootv2.models.User;
 import com.bedu.tickets_sprbootv2.repository.UserRepository;
 import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.net.URI;
 
@@ -26,4 +28,31 @@ public class UserController {
     public Iterable<User> obtenerUsuarios(){
         return repository.findAll();
     }
+
+    /*@GetMapping("/users/usuarios/{id}")
+    public ResponseEntity<User> obtenerUsuarioId(@PathVariable("id") long usrId){
+        if(usrId>10){//No exist (404 - NotFound)
+
+            //No regresa ningún mensaje
+            //return ResponseEntity.notFound().build();
+
+            //Regresa el mensaje por defecto
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "The user does not exist.");
+        }
+        if(usrId > 5){
+            //Confidencial (403 - Forbiden)
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "You do not have access permission");
+        }
+
+        User user = new User();
+        user.setName("Usuario");
+        user.setEmail("usuario@usuario.com");
+
+        return ResponseEntity.ok(user);
+    }*/
+
+    /*@PostMapping("/users/usuarios")
+    public ResponseEntity<Void> crearUnUsuario(@Valid @RequestBody User user){
+        return ResponseEntity.created(URI.create("")).build();
+    }*/
 }
